18584e0828813f2ee12870ed57bf65abd036c55e
kamapp01 <148754033+kamapp01@users.noreply.github.com>
2025-03-27 21:06:26 +0100

    made some of the view and added navigation to the next page

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -2,4 +2,18 @@
 
 public partial class MainViewModel : BaseViewModel
 {
+
+    [ObservableProperty]
+    public string btnStartText = "Start";
+
+    [ObservableProperty]
+    public string btnHowToText = "Spillets regler";
+
+
+    [RelayCommand]
+    private async Task StartGame()
+    {
+        await Shell.Current.GoToAsync("///PlayerSelectionPage");
+    }
+
 }
