16a71eb4d42402b81e55b7b031cb72cb34d9fbec
Mads Knudsen <mads@rknudsen.dk>
2025-03-28 12:39:16 +0100

    Added Binding

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,6 +6,6 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new CategoryPage(new CategoryViewModel());
+		MainPage = new AppShell();
 	}
 }
--- ViewModels/PlayerSelectionViewModel.cs
+++ ViewModels/PlayerSelectionViewModel.cs
@@ -4,6 +4,9 @@ namespace TheQuizzyApp.ViewModels;
 
 public partial class PlayerSelectionViewModel : BaseViewModel
 {
-  
-
+    [RelayCommand]
+    private async Task StartGame()
+    {
+        await Shell.Current.GoToAsync("///CategoryPage");
+    }
 }
