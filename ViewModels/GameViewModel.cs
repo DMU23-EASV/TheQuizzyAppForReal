0e11c4662d73d91d80166bfe063b77dbf8fd2313
Mads Knudsen <mads@rknudsen.dk>
2025-03-28 13:11:29 +0100

    Added remaining bindings for prototyping

--- ViewModels/GameViewModel.cs
+++ ViewModels/GameViewModel.cs
@@ -7,16 +7,9 @@ public partial class GameViewModel : BaseViewModel
 	[ObservableProperty]
 	public string message = "Click me";
 
-	[RelayCommand]
-	private void OnCounterClicked()
-	{
-		count++;
-
-		if (count == 1)
-			Message = $"Clicked {count} time";
-		else
-			Message = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(Message);
-	}
+    [RelayCommand]
+    private async Task RoundDone()
+    {
+        await Shell.Current.GoToAsync("///GameScorePage");
+    }
 }
